b0b81b933bb538382706a26403446c639689e178
eduardohartmann <58574703+eduardohartmann@users.noreply.github.com>
2025-10-22 20:38:44 -0300

    teste2

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,7 @@
 function registerServiceWorker() {
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker
-        .register("./service-worker.js")
+        .register("../service-worker.js")
         .then(() => console.log("Service Worker registrado com sucesso!"))
         .catch(err => console.error("Erro ao registrar Service Worker:", err));
     }
